d1d23a90a567ddfa992cebba211b052f5c654bdf
sabarop <sabar.pakpahan@gmail.com>
2026-04-12 08:45:32 +0700

    gta4xl-common: Infinity-X

--- common.mk
+++ common.mk
@@ -287,4 +287,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
 
 # Sign build
--include vendor/pixelos-priv/keys/keys.mk
+-include vendor/lineage-priv/keys/keys.mk
